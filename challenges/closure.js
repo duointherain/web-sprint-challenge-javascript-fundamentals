5d547d5d00ab4fe0f0281b868dc98bf79b5c8255
duointherain <duointherain@gmail.com>
2020-09-18 09:04:29 -0700

    summation

--- challenges/closure.js
+++ challenges/closure.js
@@ -21,4 +21,14 @@ myFunction();
 
 /* Task 2: Counter */
 
+function summation(n){
+  let results = 0
+  for(let i = 0; i <= n; i++){
+      results += i;
+  }
+  return results;
+}
+console.log(summation(4))
+
+
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
